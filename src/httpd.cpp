d27e04216f001b3d43e182520c042b6b8fe80ce3
Andy Doan <andy@foundries.io>
2021-01-16 22:22:50 -0600

    Make plain text server
    Signed-off-by: Andy Doan <andy@foundries.io>

--- src/httpd.cpp
+++ src/httpd.cpp
@@ -1,8 +1,38 @@
+#include <sstream>
+
 #include "cpp-httplib/httplib.h"
 
-static void show_checks(httplib::Response &res) { res.set_content("Hello World!\n", "text/plain"); }
+#include "check.h"
 
-static void show_check(const std::string &name, httplib::Response &res) { res.set_content(name + "\n", "text/plain"); }
+static void show_checks(httplib::Response &res) {
+  std::stringstream ss;
+  for (const auto &it : checks_list()) {
+    ss << std::get<0>(it) << ":\t";
+    auto sts = std::get<1>(it)->run();
+    ss << sts.valueString() << "\t";
+    ss << sts.summary << "\n";
+  }
+  res.set_content(ss.str(), "text/plain");
+}
+
+static void show_check(const std::string &name, httplib::Response &res) {
+  std::stringstream ss;
+  for (const auto &it : checks_list()) {
+    if (name == std::get<0>(it)) {
+      auto sts = std::get<1>(it)->run();
+      ss << "Name:  " << name << "\n";
+      ss << "Status:" << sts.valueString() << " " << sts.summary << "\n";
+      ss << "Logs:\n";
+      ss << std::get<1>(it)->getLog();
+      res.set_content(ss.str() + "\n", "text/plain");
+      return;
+    }
+  }
+  ss << "check(" << name << ") not found\n";
+  res.status = 404;
+  res.reason = "Not Found";
+  res.set_content(ss.str() + "\n", "text/plain");
+}
 
 int httpd_main(int port) {
   httplib::Server svr;
