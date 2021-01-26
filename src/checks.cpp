af3b066ae94e0d9150ade84cf69828c3ed7220ef
Andy Doan <andy@foundries.io>
2021-01-25 21:46:40 -0600

    Auto discover compose apps at start
    Signed-off-by: Andy Doan <andy@foundries.io>

--- src/checks.cpp
+++ src/checks.cpp
@@ -1,8 +1,18 @@
 #include "check.h"
 
+#include "boost/filesystem.hpp"
+
 std::vector<std::tuple<std::string, std::unique_ptr<CheckInterface>>> checks_list() {
   std::vector<std::tuple<std::string, std::unique_ptr<CheckInterface>>> checks;
   checks.emplace_back("docker", std::make_unique<SystemDCheck>("docker.service"));
   checks.emplace_back("aktualizr-lite", std::make_unique<SystemDCheck>("aktualizr-lite.service"));
+
+  boost::filesystem::path p("/var/sota/compose-apps");
+  for (const auto &it : boost::filesystem::directory_iterator(p)) {
+    if (boost::filesystem::is_directory(it.path())) {
+      auto app = it.path().filename().native();
+      checks.emplace_back(app, std::make_unique<ComposeCheck>(app));
+    }
+  }
   return checks;
 }
